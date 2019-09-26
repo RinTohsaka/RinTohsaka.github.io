f67abad8dd2794c1cb33c9872d687b799d0a5a2b
RinTohsaka <RinTohsaka@users.noreply.github.com>
2019-09-26 16:13:27 +0300

    Added covers for missing games

---
+++ covers.js
@@ -0,0 +1,65 @@
+var covers_data = [
+  {
+    "img": "v15199.jpg",
+    "decade": "90",
+    "released": "1996-09-06",
+    "producer": "ALTACIA",
+    "title": "真説 神谷右京3 ～Missing Link～"
+  },
+  {
+    "img": "v15197.jpg",
+    "decade": "90",
+    "released": "1997-04-25",
+    "producer": "ALTACIA",
+    "title": "探偵・苫米地研介 普通はこうだぜ!"
+  },
+  {
+    "img": "v8681.jpg",
+    "decade": "90",
+    "released": "1992-11-27",
+    "producer": "D.O.",
+    "title": "DOR Part 3"
+  },
+  {
+    "img": "v8733.jpg",
+    "decade": "90",
+    "released": "1994-03-24",
+    "producer": "Dez Climax",
+    "title": "淫獣学園 ～La★Blue Girl～"
+  },
+  {
+    "img": "v2981.jpg",
+    "decade": "00",
+    "released": "2000-03-23",
+    "producer": "KID",
+    "title": "Infinity"
+  },
+  {
+    "img": "v8105.jpg",
+    "decade": "90",
+    "released": "1996-07-26",
+    "producer": "PIL",
+    "title": "PILcaSEX"
+  },
+  {
+    "img": "v8552.jpg",
+    "decade": "90",
+    "released": "1994-12-22",
+    "producer": "RED-ZONE",
+    "title": "亜紀子Gold"
+  },
+  {
+    "img": "v3128.jpg",
+    "decade": "00",
+    "released": "2006-10-27",
+    "producer": "TOP",
+    "title": "萌えろDownhill Night 2"
+  },
+  {
+    "img": "v26130.jpg",
+    "decade": "90",
+    "released": "1997-06-06",
+    "producer": "アイス ソフト",
+    "title": "格闘コスプレお雀娘"
+  }
+];
