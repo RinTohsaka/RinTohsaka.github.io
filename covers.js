b8b32f7335c58d0d085af57070bcfd0bc8013710
RinTohsaka <RinTohsaka@users.noreply.github.com>
2019-09-27 11:07:42 +0300

    Covers index updated

--- covers.js
+++ covers.js
@@ -28,11 +28,60 @@ var covers_data = [
     "title": "淫獣学園 ～La★Blue Girl～"
   },
   {
+    "img": "v4709.jpg",
+    "decade": "90",
+    "released": "1996-11-29",
+    "producer": "Digital Frontier",
+    "title": "金田一少年の事件簿 悲報島 新たなる惨劇"
+  },
+  {
+    "img": "v18986.jpg",
+    "decade": "00",
+    "released": "2003-10-26",
+    "producer": "Flat",
+    "title": "いくものへの子守唄"
+  },
+  {
+    "img": "v22350.jpg",
+    "decade": "90",
+    "released": "1999-10-13",
+    "producer": "HYPERSPACE",
+    "title": "振まん 美貌編"
+  },
+  {
+    "img": "v15162.jpg",
+    "decade": "90",
+    "released": "1996-10-25",
+    "producer": "ILLUSION",
+    "title": "生本●"
+  },
+  {
     "img": "v2981.jpg",
     "decade": "00",
     "released": "2000-03-23",
     "producer": "KID",
     "title": "Infinity"
+  },
+  {
+    "img": "v5786.jpg",
+    "decade": "00",
+    "released": "2004-08-15",
+    "producer": "LOVERSOUL",
+    "title": "希望入りパン菓子"
+  },
+  {
+    "img": "v21865.jpg",
+    "decade": "90",
+    "released": "1997-08-15",
+    "producer": "Lunar Soft",
+    "title": "淫DAYS2"
+  },
+  {
+    "img": "v7993.jpg",
+    "decade": "90",
+    "released": "1994-11-30",
+    "producer": "May-Be SOFT",
+    "title": "FILE 国防総省 情報局 情報漏洩対策部女性尋問員"
   },
   {
     "img": "v8105.jpg",
@@ -42,11 +91,53 @@ var covers_data = [
     "title": "PILcaSEX"
   },
   {
+    "img": "v21949.jpg",
+    "decade": "90",
+    "released": "1997-07-14",
+    "producer": "PNC",
+    "title": "超天然隊"
+  },
+  {
     "img": "v8552.jpg",
     "decade": "90",
     "released": "1994-12-22",
     "producer": "RED-ZONE",
     "title": "亜紀子Gold"
+  },
+  {
+    "img": "v8959.jpg",
+    "decade": "90",
+    "released": "1995-10-06",
+    "producer": "RED-ZONE",
+    "title": "真奈美 ～愛と交換の日々～"
+  },
+  {
+    "img": "v20691.jpg",
+    "decade": "00",
+    "released": "2007-05-11",
+    "producer": "SOTS",
+    "title": "ときどき晴天を見上げて…"
+  },
+  {
+    "img": "v1893.jpg",
+    "decade": "90",
+    "released": "1998-08-07",
+    "producer": "TAKUYO",
+    "title": "e'tude prologue ～揺れ動く心のかたち～"
+  },
+  {
+    "img": "v3542.jpg",
+    "decade": "00",
+    "released": "2004-07-23",
+    "producer": "TAKUYO",
+    "title": "ほしがりエンプーサ"
+  },
+  {
+    "img": "v2817.jpg",
+    "decade": "00",
+    "released": "2005-07-21",
+    "producer": "TAKUYO",
+    "title": "MEDICAL91"
   },
   {
     "img": "v3128.jpg",
@@ -56,10 +147,101 @@ var covers_data = [
     "title": "萌えろDownhill Night 2"
   },
   {
+    "img": "v15906.jpg",
+    "decade": "00",
+    "released": "2001-11-30",
+    "producer": "URAN",
+    "title": "駅弁"
+  },
+  {
+    "img": "v24925.jpg",
+    "decade": "90",
+    "released": "1996-XX-XX",
+    "producer": "h.m.p",
+    "title": "性戦士ヤリーマン12"
+  },
+  {
+    "img": "v19979.jpg",
+    "decade": "00",
+    "released": "2002-XX-XX",
+    "producer": "hi-way",
+    "title": "性欲の虜"
+  },
+  {
+    "img": "v4505.jpg",
+    "decade": "10",
+    "released": "2010-09-24",
+    "producer": "ocelot",
+    "title": "ソード・ワールド2.0リプレイ　たのだん ～いにしえの船を追えっ！～"
+  },
+  {
+    "img": "v8966.jpg",
+    "decade": "90",
+    "released": "1995-11-XX",
+    "producer": "ぷち",
+    "title": "メモリーズ ～白い夜空に舞う天使～"
+  },
+  {
+    "img": "v24992.jpg",
+    "decade": "90",
+    "released": "1994-03-01",
+    "producer": "まんぼう小屋",
+    "title": "すたっかーと"
+  },
+  {
     "img": "v26130.jpg",
     "decade": "90",
     "released": "1997-06-06",
     "producer": "アイス ソフト",
     "title": "格闘コスプレお雀娘"
+  },
+  {
+    "img": "v3662.jpg",
+    "decade": "00",
+    "released": "2003-06-26",
+    "producer": "サイバーフロント",
+    "title": "ファンタスティックフォーチュン2"
+  },
+  {
+    "img": "v843.jpg",
+    "decade": "90",
+    "released": "1995-12-22",
+    "producer": "ティアラ",
+    "title": "迷走都市"
+  },
+  {
+    "img": "v6241.jpg",
+    "decade": "90",
+    "released": "1997-XX-XX",
+    "producer": "ファブコミュニケーションズ",
+    "title": "ラストメッセージ ～さらば愛しき女よ。～"
+  },
+  {
+    "img": "v6240.jpg",
+    "decade": "90",
+    "released": "1997-XX-XX",
+    "producer": "ファブコミュニケーションズ",
+    "title": "ラストメッセージ ～さようなら私の愛した男～"
+  },
+  {
+    "img": "v18247.jpg",
+    "decade": "90",
+    "released": "1995-01-27",
+    "producer": "ベルーガオンピュータ",
+    "title": "URM～M15惑星に生まれて～"
+  },
+  {
+    "img": "v11034.jpg",
+    "decade": "90",
+    "released": "1995-12-07",
+    "producer": "ラブ・ガン",
+    "title": "Ace of Spades"
+  },
+  {
+    "img": "v21950.jpg",
+    "decade": "90",
+    "released": "1997-08-20",
+    "producer": "東芝ＥＭＩ株式会社",
+    "title": "淫魔大都市 BEAST CITY 【ヴァンパイアマドンナ編】"
   }
 ];
